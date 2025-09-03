8827cb466e7f2104819bc957688f4d3327c7f6db
sbx_user1051 <sbx_user1051@169.254.61.23>
2025-09-03 20:38:15 +0000

    Wed Sep 03 20:38:15 UTC 2025

--- src/main/java/io/accelerate/solutions/DMO/DemoRound1Solution.java
+++ src/main/java/io/accelerate/solutions/DMO/DemoRound1Solution.java
@@ -3,19 +3,51 @@ package io.accelerate.solutions.DMO;
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class DemoRound1Solution {
+
+    /**
+     * Increment a given integer by one.
+     *
+     * @param x an integer to be incremented
+     * @return an integer incremented by one
+     */
     public int increment(int x) {
-        throw new SolutionNotImplementedException();
+        return x + 1;
     }
 
+    /**
+     * Convert all letters of the input string to uppercase.
+     *
+     * @param text the input string
+     * @return the input string converted to uppercase
+     */
     public String toUppercase(String text) {
-        throw new SolutionNotImplementedException();
+        if (text == null) {
+            throw new IllegalArgumentException("Input string cannot be null");
+        }
+        return text.toUpperCase();
     }
 
+    /**
+     * Compose a letter to Santa.
+     * Example content of the letter can be:
+     * "Dear Santa, I hope you are doing well."
+     *
+     * @return a String containing the letter to Santa
+     */
     public String letterToSanta() {
-        throw new SolutionNotImplementedException();
+        return "Dear Santa, I hope you are doing well.";
     }
 
+    /**
+     * Count the number of lines in a given multi-line string.
+     *
+     * @param text the input string containing multiple lines
+     * @return the number of lines in the input string
+     */
     public int countLines(String text) {
-        throw new SolutionNotImplementedException();
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+        return text.split("\r\n|\r|\n").length;
     }
 }
